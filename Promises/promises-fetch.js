91c05c49ab18fbd032d60ed80d299c206851bd4f
Am1d1v <dimadiman1adima@gmail.com>
2024-10-18 19:53:04 +0300

    Promise reject

--- Promises/promises-fetch.js
+++ Promises/promises-fetch.js
@@ -11,10 +11,28 @@
 //     })
 
 // Chaining
-fetch('https://dummyjson.com/products')
-    .then(data => data.json())
+// fetch('https://dummyjson.com/products')
+//     .then(data => data.json())
+//     .then(({products}) => {
+//         fetch(`https://dummyjson.com/products/` + products[0].id)
+//         .then(response => {
+//             //return 0;
+//             return response.json()
+//         })
+//         .then(data => console.log(data));
+//     });
+
+
+// Reject
+fetch('https://dummyjson.com/productstypo')
+    .then(response => {
+        console.log(response);
+        return response;
+    }, reject => console.log(reject))
     .then(({products}) => {
-        fetch(`https://dummyjson.com/products/` + products[0].id)
-        .then(data => data.json())
-        .then(data => console.log(data));
-    })
+        console.log(products)
+        return fetch(`https://dummyjson.com/productstypo/` + products[0].id)
+    }, error => console.log(error))
+        .then(response => response.json())
+        .then(data => console.log(data))
+        .catch(error => console.log(error))

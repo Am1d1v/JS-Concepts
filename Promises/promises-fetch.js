33d3ab1e85d1800cd1105a5bb14860cf98ca6ba6
Am1d1v <dimadiman1adima@gmail.com>
2024-10-18 19:11:06 +0300

    Promise chaining

--- Promises/promises-fetch.js
+++ Promises/promises-fetch.js
@@ -1,11 +1,20 @@
 
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(data => {
-        //console.log(data)
-        return data.json();
-    })
-    .then(data => {
-        data.completed = true;
-        console.log(data)
+// fetch('https://jsonplaceholder.typicode.com/todos/1')
+//     .then(data => {
+//         //console.log(data)
+//         return data.json();
+//     })
+//     .then(data => {
+//         data.completed = true;
+//         //console.log(data)
+//     })
+
+// Chaining
+fetch('https://dummyjson.com/products')
+    .then(data => data.json())
+    .then(({products}) => {
+        fetch(`https://dummyjson.com/products/` + products[0].id)
+        .then(data => data.json())
+        .then(data => console.log(data));
     })

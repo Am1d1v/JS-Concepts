2e4fd7c6117152c1be249108f389c25293fa3638
Am1d1v <dimadiman1adima@gmail.com>
2024-10-19 15:39:09 +0300

    Bid data cloud. Get current City

--- Promises/async-await.js
+++ Promises/async-await.js
@@ -1,18 +1,37 @@
 
 
-async function getProducts(){
-    try {
-        const response = await fetch(`https://dummyjson.com/productss`);
-        if(response.ok){
-            const {products} = await response.json()
-            const specificProduct = await products[0];
-            console.log(specificProduct);
-        } else {
-            throw new Error('Error Message');
-        }
+// async function getProducts(){
+//     try {
+//         const response = await fetch(`https://dummyjson.com/productss`);
+//         if(response.ok){
+//             const {products} = await response.json()
+//             const specificProduct = await products[0];
+//             console.log(specificProduct);
+//         } else {
+//             throw new Error(response.status);
+//         }
         
-    } catch (error) {
-        console.error(error);
-    }
+//     } catch (error) {
+//         console.error(error);
+//     } finally {
+//         console.log('Finally Text');
+//     }
+// }
+// getProducts();
+
+
+
+// Get current city
+async function getPosition(){
+    let longitude;
+    let latitude;
+    
+    navigator.geolocation.getCurrentPosition((pos) => {
+       longitude = pos.coords.latitude;
+       latitude = pos.coords.longitude
+    });
+    
+    const response = await fetch(`https://api.bigdatacloud.net/data/reverse-geocode-client?latitude=${latitude}&longitude=${longitude}`).then(response => response.json());
+    console.log(response.city)
 }
-getProducts();
+getPosition();
